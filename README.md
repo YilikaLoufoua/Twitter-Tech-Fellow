ea6ea38c86bfb518a9f39c9b8ab307e9f6f107ca
Yilika Loufoua <yilika.loufoua@tcu.edu>
2021-07-30 21:49:49 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Twitter - Part II
+
+This is a basic twitter app to view, compose, favorite, and retweet tweets.
+
+Time spent: **5** hours spent in total
+
+## User Stories
+
+The following **required** functionality is completed:
+
+- [x] User can compose a tweet. (4pts)
+- [x] User can favorite a tweet. (4pts)
+- [x] User can retweet a tweet. (2pts)
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories:
+
+<img src='http://g.recordit.co/CcFMkeqQNN.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+
+# Twitter - Part I
+
+This is a basic twitter app to read your tweets.
+
+Time spent: 5 hours spent in total
+
+## User Stories
+
+The following **required** functionality is completed:
+
+- [x] User sees app icon in home screen and styled launch screen.
+- [x] User can log in. 
+- [x] User can log out. 
+- [x] User stays logged in across restarts. 
+- [x] User can view tweets with the user profile picture, username, and tweet text. 
+
+The following **bonus** features are implemented:
+
+- [x] User can pull to refresh. 
+- [x] User can load past tweets infinitely. 
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories:
+
+<img src='http://g.recordit.co/IlmuexFQcQ.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
---
+++ Twitter/TweetCell.swift
@@ -0,0 +1,90 @@
+//
+//  TweetCell.swift
+//  Twitter
+//
+//  Created by Yilika Loufoua on 3/5/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    @IBOutlet weak var favButton: UIButton!
+    
+    @IBOutlet weak var retweetButton: UIButton!
+    
+    @IBAction func favoriteTweet(_ sender: Any) {
+        let toBeFavorited = !favorited
+        if (toBeFavorited) {
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorited(true)
+            }, failure: { (error) in
+                print("Favoriting did not succeed")
+            })
+        } else {
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                self.setFavorited(false)
+            }, failure: { (error) in
+                print("Unfavoriting did not succeed")
+            })
+        }
+    }
+    
+    @IBAction func retweet(_ sender: Any) {
+        let toBeRetweeted = !retweeted
+        if (toBeRetweeted) {
+            TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+                self.setRetweeted(true)
+            }, failure: { (error) in
+                print("Retweeting did not succeed")
+            })
+        } else {
+            TwitterAPICaller.client?.unRetweet(tweetId: tweetId, success: {
+                self.setRetweeted(false)
+            }, failure: { (error) in
+                print("Retweeting did not succeed")
+            })
+        }
+    }
+    
+    var favorited:Bool = false
+    var retweeted:Bool = false
+    var tweetId:Int = -1
+    
+    func setFavorited(_ isFavorited:Bool) {
+        favorited = isFavorited
+        if (isFavorited) {
+            favButton.setImage(UIImage(named:"favor-icon-red"), for: UIControl.State.normal)
+        } else {
+            favButton.setImage(UIImage(named:"favor-icon"), for: UIControl.State.normal)
+        }
+    }
+    
+    func setRetweeted(_ isRetweeted:Bool) {
+        retweeted = isRetweeted
+        if (retweeted) {
+            retweetButton.setImage(UIImage(named:"retweet-icon-green"), for: UIControl.State.normal)
+        } else {
+            retweetButton.setImage(UIImage(named:"retweet-icon"), for: UIControl.State.normal)
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
